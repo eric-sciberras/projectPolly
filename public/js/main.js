828a3e2f1226f74be5f8996d5bbb1bd3e0863db8
ericsciberras <ericsciberras@gmail.com>
2019-01-26 08:32:32 +1100

    changed path for search and added button for making new polly

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,15 @@ $.typeahead({
   order: "desc",
   minLength: 3,
   display: ["name"],
-  href:"/{{_id}}",
+  href:"/politician/{{_id}}",
  //dynamic: true,
   //delay: 500,
   hint: true,
-  emptyTemplate: "no result for {{query}}",
+  // emptyTemplate: function () {
+  //   return "<p class="lead">Can't find the Politician you are looking for :</p> <button type="button" class="btn btn-primary" href="/add/a/politician">Add a Polly</button>";
+  // },
+  emptyTemplate: "no result for {{query}}  <a class='btn btn-primary float-right' href='add/a/politician' role='button'>Add A Polly</a>",
+  // emptyTemplate: "no result for {{query}}",
   source: {
     remote: {
       url: '/search/',
