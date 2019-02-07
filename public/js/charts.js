c232ac0026a307aeba5c112a4a27052700bbbf41
ericsciberras <ericsciberras@gmail.com>
2019-02-07 17:50:07 +1100

    added jqeury request and no data present message

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,8 +1,4 @@
-// import { promises } from "fs";
-
-// window.onload = radarChartConstructor;
-
-function radarChartConstructor(politician) {
+function radarChartConstructor(averageRatings) {
   var ctx = document.getElementById("characteristicsChart").getContext("2d");
   var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -21,11 +17,11 @@ function radarChartConstructor(politician) {
           backgroundColor: "rgba(66,139,202,.5)",
           borderColor: "rgba(66,139,202,.5)",
           data: [
-            politician.averageTrustworthy,
-            politician.averageAccountable,
-            politician.averageEmpathetic,
-            politician.averageKnowledgeable,
-            politician.averageRespectful
+            averageRatings != null ? averageRatings.trustworthy : "0",
+            averageRatings != null ? averageRatings.accountable : "0",
+            averageRatings != null ? averageRatings.empathetic : "0",
+            averageRatings != null ? averageRatings.knowledgeable : "0",
+            averageRatings != null ? averageRatings.respectful : "0"
           ]
         }
       ]
@@ -61,40 +57,56 @@ function generateDoughnutCharts(promisesMade) {
   }
 }
 
-function doughnutChartConstructor(promise, index) {
-  var ctx = document.getElementById("promiseChart" + index).getContext("2d");
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: "doughnut",
+Chart.plugins.register({
+  afterDraw: function(chart) {
+    if (chart.data.datasets[0].data.length == 0) {
+      // No data is present
+      var ctx = chart.chart.ctx;
+      var width = chart.chart.width;
+      var height = chart.chart.height;
+      chart.clear();
 
-    // The data for our dataset
-    data: {
-      labels: ["Delivered", "Partially Delivered", "Broken"],
-      datasets: [
-        {
-          label: "My First dataset",
-          backgroundColor: [
-            "rgb(40,167,69)",
-            "rgb(255,193,7)",
-            "rgb(220,53,69)"
-          ],
-          //borderColor: "rgb(255, 99, 132)",
-          data: [40, 10, 5]
-          // data: [
-          //   politician.delivered,
-          //   politician.partiallyDelivered,
-          //   politician.broken
-          // ]
-        }
-      ]
-    },
-    // Configuration options go here
-    options: {}
+      ctx.save();
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.font = "16px normal 'Helvetica Nueue'";
+      ctx.fillText("No data to display", width / 2, height / 2);
+      ctx.restore();
+    }
+  }
+});
+
+function doughnutChartConstructor(promise, index) {
+  jQuery.get("/promiseData", { promiseId: promise._id }, results => {
+    var ctx = document.getElementById("promiseChart" + index).getContext("2d");
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: "pie",
+
+      // The data for our dataset
+      data: {
+        labels: ["Delivered", "Partially Delivered", "Broken"],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: [
+              "rgb(40,167,69)",
+              "rgb(255,193,7)",
+              "rgb(220,53,69)"
+            ],
+            //borderColor: "rgb(255, 99, 132)",
+            data: results
+          }
+        ]
+      },
+      // Configuration options go here
+      options: {}
+    });
   });
 }
 
 function rangeToValue(value) {
-  if (value == -1) {
+  if (value == null || value == -1) {
     return "Off";
   }
   return value;
