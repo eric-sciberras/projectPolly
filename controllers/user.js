25d45cc8e0ca2ddd10b5dd90478e2c94af9fe9c3
ericsciberras <ericsciberras@gmail.com>
2019-02-07 17:47:16 +1100

    changed email names

--- controllers/user.js
+++ controllers/user.js
@@ -352,8 +352,8 @@ exports.postReset = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: "hackathon@starter.com",
-      subject: "Your Hackathon Starter password has been changed",
+      from: "Project Polly",
+      subject: "Your Rate a Polly password has been changed",
       text: `Hello,\n\nThis is a confirmation that the password for your account ${
         user.email
       } has just been changed.\n`
@@ -466,8 +466,8 @@ exports.postForgot = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: "hackathon@starter.com",
-      subject: "Reset your password on Hackathon Starter",
+      from: "Project Polly",
+      subject: "Reset your password on Rate a Polly",
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
