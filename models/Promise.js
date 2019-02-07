84a081c76eff562b323c1e647d247cfab6999251
ericsciberras <ericsciberras@gmail.com>
2019-02-07 17:50:59 +1100

    removed necessary lines

--- models/Promise.js
+++ models/Promise.js
@@ -1,40 +1,29 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const PromiseSchema = new mongoose.Schema(
   {
-    user_id: { type: String, ref: "User", required: true },
-    politician_id: { type: String, ref: "Politician", required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    politician: {
+      type: String,
+      ref: "Politician",
+      required: true
+    },
     title: { type: String, required: true },
     description: { type: String, required: true },
     source: { type: String, required: true },
 
-    // One promise can have many votes from other users
-    // Votes allow users to decide If a promise was
-    // delivered, partially delivered or broken
-    votes: [
-      {
-        user_id: { type: String, ref: "User", required: true },
-        vote: Number
-      }
-    ],
+    promiseVote: [{ type: mongoose.Schema.Types.ObjectId, ref: "PromiseVote" }],
 
-    // Users can upvote or downvote the promise entry itself if they feel
-    // It contains errors or is biased
-    repuation: [
+    reputationVote: [
       {
-        user_id: { type: String, ref: "User", required: true },
-        reputation: Number
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "PromiseReputationVote"
       }
     ]
   },
   { timestamps: true }
 );
-
-// just a reminder that i need to set these to enable these so the
-// virtual properties (if any) are included when I retrieve a promise
-
-// PromiseSchema.set("toObject", { virtuals: true });
-// PromiseSchema.set("toJSON", { virtuals: true });
 
 const Promise = mongoose.model("Promise", PromiseSchema);
 
